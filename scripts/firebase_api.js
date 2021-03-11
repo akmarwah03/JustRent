999dd3876660c667c1af118cfdf30ca3df8c68ad
Akshay Marwah <amarwah4@my.bcit.ca>
2021-03-12 04:22:00 +0530

    Fixed bottom nav

--- scripts/firebase_api.js
+++ scripts/firebase_api.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyBS86ESDp5eMJSOjntWxZmmGtgKscNpchs",
-    authDomain: "justrent-fff6d.firebaseapp.com",
-    projectId: "justrent-fff6d",
-    storageBucket: "justrent-fff6d.appspot.com",
-    messagingSenderId: "319570124134",
-    appId: "1:319570124134:web:ff0a75672a6dd4c14afca7"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore(); //add this to read and write
+  apiKey: "AIzaSyBS86ESDp5eMJSOjntWxZmmGtgKscNpchs",
+  authDomain: "justrent-fff6d.firebaseapp.com",
+  projectId: "justrent-fff6d",
+  storageBucket: "justrent-fff6d.appspot.com",
+  messagingSenderId: "319570124134",
+  appId: "1:319570124134:web:ff0a75672a6dd4c14afca7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore(); //add this to read and write
