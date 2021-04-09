5bcb86cc0cf4843f392a4cf751a1158846c90436
Akshay Marwah <amarwah4@my.bcit.ca>
2021-04-09 07:51:30 -0700

    Checkout loads data from cart and on submit deletes it from cart and stores in orders

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,13 +1,51 @@
-$(document).ready(() => {
-    $('#db').click(()=>{
-        localStorage.setItem('category',"db");
-    });
+var total = 0;
 
-    $('#wp').click(()=>{
-        localStorage.setItem('category',"wp");
-    });
+function getProduct() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            var users = db.collection("cart").where("user", "==", user.uid);
+            users.get()
+                .then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        let id = doc.data().pid;
+                        let week = doc.data().week;
+                        db.collection("equipments")
+                            .doc(id)
+                            .get()
+                            .then(function (doc) {
+                                var name = doc.data().name;
+                                var cost = doc.data().cost;
+                                var calcCost = week * cost;
+                                total += calcCost;
+                                let code = $('<div class="item"><span class="price">$' + calcCost + '</span><p class="item-name">' + name + '</p><p class="item-description">' + doc.data().description + '</p></div>');
+                                $(".products .title").after(code);
+                                $(".total .price").html("$" + total);
+                            })
 
-    $('#kb').click(()=>{
-        localStorage.setItem('category',"kb");
+                    });
+
+                });
+        } else {
+            console.log("Login in first");
+        }
     });
-});
+}
+
+getProduct();
+
+$(".checker").click(() => {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            var users = db.collection("cart").where("user", "==", user.uid);
+            users.get().then(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
+                    db.collection("orders").doc(doc.uid).set(doc.data());
+                    doc.ref.delete();
+                });
+            });
+
+        } else {
+            console.log("Login in first");
+        }
+    });
+})
